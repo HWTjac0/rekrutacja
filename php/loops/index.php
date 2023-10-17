1e3bf7d1de3cc8ad637754ed57130fc7ebe0f771
jacmill <jakub3020.jmy@gmail.com>
2023-10-17 21:19:45 +0200

    PHP: Loop ala fizzbuzz task -> done

--- php/loops/index.php
+++ php/loops/index.php
@@ -8,3 +8,13 @@
 // If the number is a multiple of 4, write Fire instead of the number
 // If the number is a multiple of 7, write Boom instead of the number
 // If the number is a multiple of 10, repeat it 10 times in the same line in red
+// Nie było napisane co zrobić jeśli np. liczba jest i wielokrotnością 10 i 4 lub 4 i 7 więc zostawiej tak jak jest. :)
+for($i = 100; $i >= 0; $i--) {
+  $output = match(0) {
+    $i % 4  => "Fire",
+    $i % 7  => "Boom",
+    $i % 10 => "<span style='color: red'>" . str_repeat($i . " ", 10) . "</span>",
+    default => $i,
+  };
+  echo $output . "</br>";
+}
