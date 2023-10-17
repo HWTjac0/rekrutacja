215abd13f1b6c90edce3ef39c7e5731af951ef4f
jacmill <jakub3020.jmy@gmail.com>
2023-10-17 22:07:49 +0200

    JS: Riddle task -> done

--- js/riddles/index.js
+++ js/riddles/index.js
@@ -1,7 +1,7 @@
 // 1. odwróć liczbę
 // np dla 12345, funkcja powinna zwrócić 54321
-function reverseNumber() {
-  // uzupełnij tutaj
+function reverseNumber(num) {
+  return parseInt([...num.toString()].reverse().join(""));
 }
 
 console.log("1.", reverseNumber(12345));
@@ -9,8 +9,9 @@ console.log("1.", reverseNumber(12345));
 // 2. doodaj do siebie wszystkie wartości z tablicy, które są parzyste
 // dla tablicy tab powinniśmy otrzymać 2 + 4 + 6 + 8 = 20
 const tab = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const isEven = num => num % 2 == 0;
 function addEven(array) {
-  // uzupełnij tutaj
+  return array.reduce((acc, cur) => isEven(cur) ? acc + cur : acc, 0);
 }
 
 console.log("2.", addEven(tab));
