8a4ef0b141fc4c4991d0cd4b48b6b0e238e3066e
jacmill <jakub3020.jmy@gmail.com>
2023-10-17 22:48:09 +0200

    JS: Tic Tac Toe task -> done

--- js/tic-tac-toe/index.js
+++ js/tic-tac-toe/index.js
@@ -13,7 +13,7 @@ board.addEventListener("click", ({ target }) => {
   target.classList.add(`tile-${turn}`);
   symbols[idx % 3][Math.floor(idx / 3)] = turn;
   turn = turn === "x" ? "o" : "x";
-
+  console.log(symbols);
   displayTurn(turn);
 
   checkWin();
@@ -21,13 +21,36 @@ board.addEventListener("click", ({ target }) => {
 
 function displayTurn(turn) {
   // 1. zmień text elementu h1 z klasą "turn" zależnie od tego, czyja jest aktualnie tura
+  document.querySelector(".turn").textContent = `${turn.toUpperCase()} turn`; 
+}
+
+function checkLine(turn, first, second, third) {
+  return first === turn && second === turn && third === turn;  
 }
 
 function checkWin() {
   // 2. sprawdź czy któryś z graczy wygrał pojedynek - jeśli tak wyświetla komunikat (możesz użyć np. funkcji "alert(...)")
+  for(let i = 0; i < 3; i++) {
+    if(checkLine(turn, symbols[0][i], symbols[1][i], symbols[2][i]) ||
+       checkLine(turn, symbols[i][0], symbols[i][1], symbols[i][2]) ||
+       checkLine(turn, symbols[0][0], symbols[1][1], symbols[2][2]) ||
+       checkLine(turn, symbols[0][2], symbols[1][1], symbols[2][0])
+    ){
+      reset()
+      alert(`Wygrywa ${turn.toUpperCase()}`)
+      return;
+    }
+  }  
 }
 
 // 3. dodaj listener pod przycisk z napisaem "reset" tak, aby po jego kliknięciu wywołać funkcję reset
 function reset() {
   // 4. zresetuj stan gry
+  symbols = new Array(3).fill(null).map(_ => new Array(3).fill(""));
+  turn = "x";
+  for(const tile of tiles) {
+    tile.classList = ["tile"];
+  }
+  displayTurn(turn);
 }
+document.querySelector(".reset").addEventListener("click", reset)
